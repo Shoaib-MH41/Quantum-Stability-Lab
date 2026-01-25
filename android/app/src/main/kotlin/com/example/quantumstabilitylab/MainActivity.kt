3ef10ce6241da48d04f68d50c15eb0e30b35dc92
Shoaib-MH41 <princeshoaib2122@gmail.com>
2026-01-26 01:59:28 +0530

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/quantumstabilitylab/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.quantumstabilitylab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // خالی رہنے دے - Flutter خود ہینڈل کرے گا
+}
