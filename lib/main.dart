9260f01cf7673a48569c157463802dfbe05c0901
Shoaib-MH41 <princeshoaib2122@gmail.com>
2026-01-26 09:19:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,82 @@
 import 'package:flutter/material.dart';
 import 'ui/dashboard.dart';
+import 'ui/multi_quantum_dashboard.dart';
 
 void main() {
-  runApp(QuantumStabilityLabApp());
+  runApp(const QuantumStabilityLabApp());
 }
 
 class QuantumStabilityLabApp extends StatelessWidget {
+  const QuantumStabilityLabApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Quantum Stability Lab',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        useMaterial3: true,
       ),
-      home: Dashboard(),
-      debugShowCheckedModeBanner: false,
+      home: const MainHomeScreen(),
+    );
+  }
+}
+
+class MainHomeScreen extends StatefulWidget {
+  const MainHomeScreen({super.key});
+
+  @override
+  State<MainHomeScreen> createState() => _MainHomeScreenState();
+}
+
+class _MainHomeScreenState extends State<MainHomeScreen> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    const Dashboard(),
+    const MultiQuantumDashboard(),
+  ];
+
+  static final List<String> _appBarTitles = <String>[
+    'Quantum Stability Lab',
+    '10-Quantum Experiment',
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_appBarTitles[_selectedIndex]),
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Basic Test',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.science),
+            label: '10-Quantum',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+      ),
     );
   }
 }
