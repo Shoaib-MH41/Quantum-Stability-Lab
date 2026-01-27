3bfe334eece3a94c4a2cc14de2530153c2dba7e2
Shoaib-MH41 <princeshoaib2122@gmail.com>
2026-01-27 08:33:19 +0530

    Update dashboard.dart

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import '../core/real_quantum_particle.dart';
 
-class Dashboard extends StatefulWidget {
+class MultiQuantumDashboard extends StatefulWidget {
   @override
   _MultiQuantumDashboardState createState() => _MultiQuantumDashboardState();
 }
